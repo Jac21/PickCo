909c1375a405e2aec3bfe7218193aed9b00a7987
bote795 <nbotell2@gmail.com>
2016-04-16 19:33:46 -0500

    color api service with crud setup

---
+++ PickCo/www/js/colorSrvApi.js
@@ -0,0 +1,15 @@
+(function() {
+	'use strict';
+	angular.module('app').service('colorSrvApiCtrl',
+	[ '$resource', colorSrvApiCtrl ])
+	function colorSrvApiCtrl(){
+		var domain = "159.203.93.46:3000";
+		return $resource('https://'+domain+'/api/colors/:id',
+		 { id: '@_id' }, {
+		update: {
+		  method: 'PUT' // this method issues a PUT request
+		}
+		});
+		
+	}
+})();
